05458af75dec8c6c5368def3be9f07ff31157c98
markow <mariusz.grzegorz.kowalski@gmail.com>
2024-03-05 11:53:18 +0100

    Add DOTWEEN methods to animate UIHighlighter

--- Assets/Scripts/UI/UIHighlighter.cs
+++ Assets/Scripts/UI/UIHighlighter.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 
 namespace markow
@@ -9,18 +10,37 @@ namespace markow
     [RequireComponent(typeof(Image))]
     public class UIHighlighter : MonoBehaviour
     {
+        private Image img;
+        private float alphaValue;
+
         private void Awake()
         {
-            
+            img = GetComponent<Image>();
+            alphaValue = 0f;
+        }
+
+        private void Start()
+        {
+            Hide();
         }
 
         public void Show()
         {
+            DOTween.To(() => alphaValue, x => alphaValue = x, .5f, .25f)
+            .OnUpdate(() => {
+                img.color = new Color(1, 1, 1, alphaValue);
+            });
+
             Debug.Log("[UIHighlighter] Show");
         }
 
         public void Hide() 
         {
+            DOTween.To(() => alphaValue, x => alphaValue = x, 0f, .25f)
+            .OnUpdate(() => {
+                img.color = new Color(1, 1, 1, alphaValue);
+            }); 
+
             Debug.Log("[UIHighlighter] Hide");
         }
 
