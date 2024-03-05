0668baf987b6857a2b9361132fe552d612b6dc6d
markow <mariusz.grzegorz.kowalski@gmail.com>
2024-03-05 12:17:28 +0100

    Add properties to GridItemSO template that will allow to create configuration files combining information about UI and Prefab

--- Assets/Scripts/ScriptableObjectTemplates/GridItemSO.cs
+++ Assets/Scripts/ScriptableObjectTemplates/GridItemSO.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.EventSystems.EventTrigger;
 
 namespace markow
 {
+    [CreateAssetMenu(fileName = "GridItem", menuName = "GridMenu/GridItem")]
     public class GridItemSO : ScriptableObject
     {
-
+        public new string name;
+        public Sprite sprite;
+        public GameObject prefab;
     }
 }
