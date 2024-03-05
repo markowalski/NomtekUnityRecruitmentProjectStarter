a30fda092b1744f2a0559d5c541db0842a165ec3
markow <mariusz.grzegorz.kowalski@gmail.com>
2024-03-05 20:38:03 +0100

    Add logic to the Cube class depending on its states. SetState method overrides inherited virtual method from Entity class

--- Assets/Scripts/SceneObjects/Cube.cs
+++ Assets/Scripts/SceneObjects/Cube.cs
@@ -6,6 +6,39 @@ namespace markow
 {
     public class Cube : Entity
     {
+        public override void SetState(ENTITY_STATE _state)
+        {
+            state = _state;
 
+            switch (state)
+            {
+                case ENTITY_STATE.Disabled:
+                    break;
+                case ENTITY_STATE.Initialized:
+                    OnInitializedStateEnter();
+                    break;
+                case ENTITY_STATE.Detached:
+                    OnDetachedStateEnter();
+                    break;
+                case ENTITY_STATE.Destroyed:
+                    OnDestroyedStateEnter();
+                    break;
+            }
+        }
+
+        private void OnInitializedStateEnter()
+        {
+            
+        }
+
+        private void OnDetachedStateEnter()
+        {
+            
+        }
+
+        private void OnDestroyedStateEnter()
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/SceneObjects/Entity.cs
+++ Assets/Scripts/SceneObjects/Entity.cs
@@ -7,5 +7,16 @@ namespace markow
     public class Entity : MonoBehaviour
     {
         public ENTITY_TYPE type;
+        protected ENTITY_STATE state = ENTITY_STATE.Disabled;
+
+        public virtual void SetState(ENTITY_STATE _state)
+        {
+
+        }
+
+        public ENTITY_STATE GetState()
+        {
+            return state;
+        }
     }
 }
