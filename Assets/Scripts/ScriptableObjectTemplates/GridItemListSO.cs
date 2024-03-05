9707997d3307c4b2fbd90aec40e5cb4416a6e731
markow <mariusz.grzegorz.kowalski@gmail.com>
2024-03-05 12:12:09 +0100

    Add a list of structs to GridItemListSO that will contain Title and Content (the content will be the configuration file of a specific object)

--- Assets/Scripts/ScriptableObjectTemplates/GridItemListSO.cs
+++ Assets/Scripts/ScriptableObjectTemplates/GridItemListSO.cs
@@ -1,8 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GridItemListSO : ScriptableObject
+namespace markow
 {
+    [CreateAssetMenu(fileName = "GridItemList", menuName = "GridMenu/GridItemList")]
+    public class GridItemListSO : ScriptableObject
+    {
+        [SerializeField]
+        private List<GridItemListField> gridItemList;
 
+        [Serializable]
+        public struct GridItemListField
+        {
+            public string title;
+            public GridItemSO content;
+        }
+    }
 }
