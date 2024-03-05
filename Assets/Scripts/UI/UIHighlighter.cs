203218ac165492e94c5bba9e1bf235829b6f815f
markow <mariusz.grzegorz.kowalski@gmail.com>
2024-03-05 11:14:23 +0100

    Add a UIHighlighter object to the GridItem which will enable the item to be highlighted on mouse hover

---
+++ Assets/Scripts/UI/UIHighlighter.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace markow
+{ 
+    [RequireComponent(typeof(Image))]
+    public class UIHighlighter : MonoBehaviour
+    {
+        private void Awake()
+        {
+            
+        }
+
+        private void Show()
+        {
+            
+        }
+
+        private void Hide() 
+        {
+            
+        }
+
+    }
+}
