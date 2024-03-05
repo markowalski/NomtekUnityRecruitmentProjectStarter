e3feb572b65aa946f81eaac8ca553eac15ff10dd
markow <mariusz.grzegorz.kowalski@gmail.com>
2024-03-05 12:53:09 +0100

    Prepare the GridItem class to inject data from GridItemCFG

--- Assets/Scripts/UI/GridItem.cs
+++ Assets/Scripts/UI/GridItem.cs
@@ -1,13 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 namespace markow
 {
     public class GridItem : MonoBehaviour
     {
+        private Image img;
+        private TMPro.TextMeshProUGUI desc;
+
+        public void Setup(GridItemSO _gridItemSO)
+        {
+            img.sprite = _gridItemSO.sprite;
+            desc.text = _gridItemSO.name;
+        }
+
         public void OnPointerClickedHandler()
         {
+
         }
     }
 }
