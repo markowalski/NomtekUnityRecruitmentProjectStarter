46f172cd909fb11f0a607928ad562dd7efe75558
markow <mariusz.grzegorz.kowalski@gmail.com>
2024-03-05 20:49:00 +0100

    Add state support to the CubeEater class

--- Assets/Scripts/SceneObjects/CubeEater.cs
+++ Assets/Scripts/SceneObjects/CubeEater.cs
@@ -6,6 +6,39 @@ namespace markow
 {
     public class CubeEater : Entity
     {
+        public override void SetState(ENTITY_STATE _state)
+        {
+            state = _state;
 
+            switch (state)
+            {
+                case ENTITY_STATE.Disabled:
+                    break;
+                case ENTITY_STATE.Initialized:
+                    OnInitializedStateEnter();
+                    break;
+                case ENTITY_STATE.Detached:
+                    OnDetachedStateEnter();
+                    break;
+                case ENTITY_STATE.Destroyed:
+                    OnDestroyedStateEnter();
+                    break;
+            }
+        }
+
+        private void OnInitializedStateEnter()
+        {
+            Debug.Log("[CubeEater] OnInitializedStateEnter");
+        }
+
+        private void OnDetachedStateEnter()
+        {
+            Debug.Log("[CubeEater] OnDetachedStateEnter");
+        }
+
+        private void OnDestroyedStateEnter()
+        {
+            Destroy(gameObject);
+        }
     }
 }
