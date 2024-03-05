008cc1467841cfafdd17e09447267e2d4d12b1c7
markow <mariusz.grzegorz.kowalski@gmail.com>
2024-03-05 19:30:04 +0100

    Add functionality to EntityPlacer. The newly created object is moved to the place where the Raycast hits the Floor object

--- Assets/Scripts/EntityPlacer.cs
+++ Assets/Scripts/EntityPlacer.cs
@@ -6,6 +6,63 @@ namespace markow
 {
     public class EntityPlacer : MonoBehaviour
     {
+        private GameObject obj;
+        private Camera mainCamera;
+        private bool isCubeAttached = false;
 
+        void Start()
+        {
+            mainCamera = Camera.main;
+        }
+
+        public void Init(GameObject _obj)
+        {
+            obj = _obj;
+
+            if (!isCubeAttached) AttachCubeToCursor();
+        }
+
+
+        private void Update()
+        {
+            if (isCubeAttached && Input.GetMouseButtonDown(0))
+            {
+                DetachCube();
+            }
+
+
+            if (isCubeAttached && obj != null)
+            {
+                MoveCubeWithCursor();
+            }
+        }
+
+        private void AttachCubeToCursor()
+        {
+            if (obj != null)
+            {
+                isCubeAttached = true;
+            }
+        }
+
+        private void MoveCubeWithCursor()
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                if (hit.collider.gameObject.CompareTag("Floor"))
+                {
+                    obj.transform.position = hit.point + new Vector3(0, obj.transform.localScale.y / 2, 0);
+                }
+            }
+        }
+
+        private void DetachCube()
+        {
+            isCubeAttached = false;
+            obj = null;
+        }
     }
 }
